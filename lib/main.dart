4b7fe59c1b65c585255270335351fa4faf70ff07
shad <shadislam999@gmail.com>
2024-03-14 09:52:12 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+   MyHomePage({super.key});
+   
+   String _currentFortune = "";
+
+ 
+
+  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final _fortuneList = [
+    'You will finad a new friend',
+    'You will finad a new friend tomorrow',
+    'A truly friend love you',
+    'life is awesome when you read quran'
+
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+        title: Text("Flutter Demo"),
+      ),
+      body: Center(
+       
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             Text(
+              'your fortune is:'
+            ),
+            Text(
+               "${_fortuneList[_counter % _fortuneList.length]}",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
